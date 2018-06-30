5a75125258e6146f6b68e8df8dc9d4514f11449f
Maycon Miranda <mayconjr_@live.com>
2018-06-30 11:46:44 -0300

    escrevendo melhorar custo

--- Program.cs
+++ Program.cs
@@ -7,21 +7,23 @@ namespace economia
     {
         static void Main(string[] args)
         {
-            Produto p = new Produto("Arroz Tipo 1", "Arroz", 10.50m);
+            Produto p = new Produto("Arroz Tipo 1", "Arroz", 12);
             Compra c = new Compra(50);
             c.Compras.Add(p);
-            p = new Produto("Feijao Tipo 1", "Feijao", 11);
+            p = new Produto("Feijao Tipo 1", "Feijao", 12);
             c.Compras.Add(p);
-            p = new Produto("Arroz Tipo 2", "Arroz", 12);
+            p = new Produto("Arroz Tipo 2", "Arroz", 11);
             c.Compras.Add(p);
-            p = new Produto("Feijao Tipo 2", "Feijao", 14);
+            p = new Produto("Feijao Tipo 2", "Feijao", 11);
             c.Compras.Add(p);
-            p = new Produto("Arroz Tipo 3", "Arroz", 13);
+            p = new Produto("Arroz Tipo 3", "Arroz", 10);
+            c.Compras.Add(p);
+            p = new Produto("Feijao Tipo 3", "Arroz", 10);
             c.Compras.Add(p);
             Console.WriteLine("Compra: " );
             c.Compras.ForEach(Console.WriteLine);
             Console.WriteLine("Filtro Arroz: ");
-            c.Filter("Arroz").ForEach(Console.WriteLine);
+            c.Filter(c.Compras,"Arroz").ForEach(Console.WriteLine);
         }
     }
 }
